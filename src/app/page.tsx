3d3d5411c4ec9a9565c4854b89c7feb352d58c7f
khan-shuvo <shuvo2004@outlook.com>
2026-03-03 16:14:11 +0600

    feat(home): add filter buttons for filter categorys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,79 @@
-'use client'
+"use client";
 
+import { useBlogcontext } from "@/context/BlogDataContext";
 import { Search } from "lucide-react";
 import { motion } from "motion/react";
+import { button } from "motion/react-client";
+import { useState } from "react";
 export default function Home() {
+  const { data } = useBlogcontext();
+
+  const [selectedCategory, setSelectedCategory] = useState<string>("All");
+
+  const category = [
+    "All",
+    ...Array.from(new Set(data.map((post) => post.category))),
+  ];
   return (
     <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-6 py-8">
       <motion.div
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: .6 }}
+        transition={{ duration: 0.6 }}
         className="text-center mb-9"
       >
-        <h1 className="text-4xl md:text-5xl lg:text-6xl mb-4">Welcom to DevBlog</h1>
-        <p className="text-xl text-gray-600 max-w-2xl mx-auto">Insights on web development, design, and productivity</p>
+        <h1 className="text-4xl md:text-5xl lg:text-6xl mb-4">
+          Welcom to DevBlog
+        </h1>
+        <p className="text-xl text-gray-600 max-w-2xl mx-auto">
+          Insights on web development, design, and productivity
+        </p>
       </motion.div>
 
       {/* search bar */}
       <motion.div
-        initial={{ opacity: 0, scale: .95 }}
-        animate={{opacity: 1, scale: 1}}
-        transition={{duration: .4,delay: .2}}
-        className="relative max-w-xl mx-auto">
-          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={19} />
-          <input type="text"
+        initial={{ opacity: 0, scale: 0.95 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.4, delay: 0.2 }}
+        className="relative max-w-xl mx-auto"
+      >
+        <Search
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+          size={19}
+        />
+        <input
+          type="text"
           placeholder="Search Artecale.."
-          className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent" />
+          className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+        />
       </motion.div>
 
+      {/* filter button  */}
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="flex justify-center flex-wrap gap-3 pt-5"
+      >
+        {category.map((category, index) => (
+          <motion.button
+            key={index}
+            initial={{ opacity: 0, y: 10 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.4 + index * 0.05 }}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            onClick={() => setSelectedCategory(category)}
+            className={`px-4 py-2 rounded-full text-sm transition-colors ${selectedCategory === category
+                ? "bg-blue-600 text-white"
+                : "bg-white text-gray-700 hover:bg-gray-100"
+              }`}
+          >
+            {category}
+          </motion.button>
+        ))}
+      </motion.div>
     </div>
-  )
+  );
 }
