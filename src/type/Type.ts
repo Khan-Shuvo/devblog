0ffbfb16102b98b9aaba899f9daf069798888b5b
khan-shuvo <shuvo2004@outlook.com>
2026-03-03 15:17:59 +0600

    created a type for blog posts

---
+++ src/type/Type.ts
@@ -0,0 +1,11 @@
+export type BlogPost = {
+    id: number;
+    title: string;
+    excerpt: string
+    content: string
+    author: string
+    date: string
+    readTime: string
+    imageUrl: string
+    category: string
+}
